5f12c7caae835e35a4ad500d070f320769116513
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-02 19:35:49 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,6 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice()
-puts meal_choice(meat)
+def meal_choice(a, b = "meat")
 end
